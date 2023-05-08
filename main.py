b3f9fa478448e1b499a45a51b0c7522a3f7a31f4
James Aules <james_aules@icloud.com>
2023-05-07 23:36:37 -0400

    firs commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# Write a Python function called max_num()to find the maximum of three numbers.
+
+def maxNum(a, b, c):
+    values = [a, b, c]
+    return max(values)
+
+print(maxNum(7, 10, 2))
+
+
+# Write a Python function called mult_list() to multiply all the numbers in a list.
